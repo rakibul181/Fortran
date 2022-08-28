91f47223c7f6fd203c6df65a8c58b42fa443a3d8
rakibul181 <rakibulhasan0641@gmail.com>
2022-08-28 09:49:32 +0600

    2nd program

---
+++ addNumberIntReal.f95
@@ -0,0 +1,19 @@
+program addNumIntReal
+    implicit none
+    real:: a, b, realResult
+
+    integer:: x, y , iniResult
+
+    a = 40
+    b = 34.45
+    x = 45.8
+    y = 45.3
+
+    realResult =  a + b
+    iniResult = x + y
+
+    print *, realResult
+    print *, iniResult
+    
+end program addNumIntReal
+! real:: a, b, resul
