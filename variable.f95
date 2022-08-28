567a7d1035b7ec83ee9b79a3c9f542b09e29777d
rakibul181 <rakibulhasan0641@gmail.com>
2022-08-28 20:42:54 +0600

    variable exploreed

---
+++ variable.f95
@@ -0,0 +1,19 @@
+program variableTest
+   implicit none
+
+   integer:: a
+   real:: b
+   complex:: com
+   logical:: done
+   character:: string
+
+   a = 23
+   b = 12.345
+   com = (23, -45)
+   string = 'Helloo, Here is Rakibul Hasan'
+
+   print *, a
+   print *, b
+   print *, com
+   print *, string
+end program variableTes
